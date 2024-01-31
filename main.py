a8bb14c35f8c136e9d6dcbf8a2d44aa16971c290
Fergus Gault <fergus@gault.com>
2024-01-31 21:43:10 +0000

    minor changes and cleanup

--- main.py
+++ main.py
@@ -7,37 +7,37 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 CLOCK = pygame.time.Clock()
 
-CIRC_RAD = 30
-
 all_nodes = pygame.sprite.Group()
+
+CIRC_RAD = 30
+LINE_WIDTH = 5
 
 def update():
     running = True
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
                 break
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
                 break
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 newNode = nodes.Node(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], CIRC_RAD, len(all_nodes.sprites()))
                 # Check for collisions without removing sprites
                 collide = pygame.sprite.spritecollide(newNode, all_nodes, dokill=False)
                 # If there are no collisions, draw the node
-                print("We are here")
                 if not collide:
-                    print("We here")
                     newNode._draw_node()
                     # Add the new node to the group
                     all_nodes.add(newNode)
                 else:
                     newNode._remove_node()
-
                 break
 
         # Update the display outside of the event loop
+        all_nodes.update(events)
         pygame.display.flip()
         CLOCK.tick(30)
             
@@ -45,6 +45,7 @@ def update():
 
 def display():
     pygame.init()
+    pygame.display.set_caption("Dijkstra's Visualiser")
     update()
 
 if __name__ == "__main__":
